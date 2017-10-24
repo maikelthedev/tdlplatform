deb67c6f792d9ae760fd3827fb7f2be6607c2474
maikel <maikel@maikel.uk>
2017-10-24 12:44:35 +0100

    Initial creator for the tests and its unittests

---
+++ creator.py
@@ -0,0 +1,4 @@
+# This creates new tests and stores them in a remote mongdodb.
+
+import click
+
---
+++ tests/test_creator.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestCreatorMethods(unittest.TestCase):
+
+    def test_nothing(self):
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
